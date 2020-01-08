fe76f5a71c6a36cc304b35ab3686bd994e0c58c0
YaBoiRayden <Tradderley@gmail.com>
2020-01-07 22:36:55 -0500

    initial setup, character setup

---
+++ GrappleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] FeetCheck feet;
+    public Vector2 curVel;
+    public Vector2 dirInput;
+
+    [SerializeField] bool grounded;
+    [SerializeField] float groundSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        feet = GetComponent<FeetCheck>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = curVel;
+        grounded = feet.grounded;
+
+        if(grounded == false)
+        {
+            curVel.y -= Time.deltaTime;
+        }
+        else
+        {
+            curVel.y = 0;
+        }
+
+        dirInput.x = Input.GetAxis("Horizontal");
+        dirInput.y = Input.GetAxis("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        curVel = new Vector3(dirInput.x * groundSpeed, curVel.y);
+    }
+}
