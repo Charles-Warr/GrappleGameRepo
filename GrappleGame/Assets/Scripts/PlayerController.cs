3e037ca733877e31b639254bc6f43bba24e6dcc8
YaBoiRayden <Tradderley@gmail.com>
2020-01-09 18:27:49 -0500

    menus and stuff

--- GrappleGame/Assets/Scripts/PlayerController.cs
+++ GrappleGame/Assets/Scripts/PlayerController.cs
@@ -8,42 +8,64 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject feet;
 
     Vector2 dirInput;
+    bool dashInput;
     [SerializeField] Vector2 curVel;
 
     float gravity = 9.8f;
     [SerializeField] bool grounded;
     [SerializeField] float groundSpeed;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float startDashTime;
+    [SerializeField] float curDashTime;
+    [SerializeField] bool grabbing;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-     
+        curDashTime = startDashTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Input
-        dirInput.x = Input.GetAxis("Horizontal");
-        dirInput.y = Input.GetAxis("Vertical");
-
+        {
+            dirInput.x = Input.GetAxis("Horizontal");
+            dirInput.y = Input.GetAxis("Vertical");
+            dashInput = Input.GetKeyDown(KeyCode.LeftShift);
+        }
+        //ground check
+        {
+            grounded = feet.GetComponent<FeetCheck>().grounded;
+            if (grounded == true)
+            {
+                curVel.y = 0;
+            }
+            else
+            {
+                curVel.y -= gravity * Time.deltaTime;
+            }
+        }
         rb.velocity = curVel;
 
-        grounded = feet.GetComponent<FeetCheck>().grounded;
-        if (grounded == true)
-        {
-            curVel.y = 0;
-        }
-        else
-        {
-            curVel.y -= gravity * Time.deltaTime;
-        }
+
 
     }
 
     void FixedUpdate()
     {
+        //movement
         curVel = new Vector3(dirInput.x * groundSpeed, curVel.y);
+
+        if (dashInput && curDashTime == startDashTime)
+        {
+            Dash();
+        }
+    }
+
+    void Dash()
+    {
+        curVel = new Vector2(dashSpeed, 0);
     }
 }
