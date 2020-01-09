ac08280859f1082667ab4159e5aceefe5185ba6a
YaBoiRayden <Tradderley@gmail.com>
2020-01-09 16:51:47 -0500

    TextMeshPro add and Main Menu add

---
+++ GrappleGame/Assets/Scripts/Menus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    [SerializeField] GameObject main;
+    [SerializeField] GameObject options;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionsMenu()
+    {
+        main.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void Back()
+    {
+        options.SetActive(false);
+        main.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
