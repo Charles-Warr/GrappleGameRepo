bf538d34d35a7c7dbd9bc223d575ab52a0d602f4
Charles-Warr <cawarr@student.fullsail.edu>
2020-01-10 14:59:39 -0500

    Updated CameraFollow Script
    updated the script for the Camera Follow

--- GrappleGame/Assets/Scripts/CameraFollow.cs
+++ GrappleGame/Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] GameObject player;
+    private GameObject player;
 
     [SerializeField] Vector3 camVec;
     [SerializeField] float zDist;
@@ -13,20 +13,23 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     void LateUpdate()
     {
+       /*
         transform.position = camVec;
         camVec.x = player.transform.position.x + offset.x;
         camVec.y = player.transform.position.y + offset.y;
         camVec.z = zDist;
+        */
+        this.transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, zDist );
     }
 }
