2cccd477e2d16ba58cf83d334bab68e062cd78d9
Charles-Warr <cawarr@student.fullsail.edu>
2020-01-16 02:29:20 -0500

    Camera Follow Script Updated

--- GrappleGame/Assets/Scripts/CameraFollow.cs
+++ GrappleGame/Assets/Scripts/CameraFollow.cs
@@ -6,43 +6,59 @@ public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
 
-    [SerializeField] Vector3 camVec;
-    [SerializeField] float zDist;
     [SerializeField] Vector3 offset;
-    [SerializeField] float delay;
+    [SerializeField] float smoothSpeed;
+    [SerializeField] float speedSmoothSpeed;
+    [SerializeField] float xMin;
+    [SerializeField] float xMax;
+    [SerializeField] float yMin;
+    [SerializeField] float yMax;
 
-    // Start is called before the first frame update
+    private bool moveCamera;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        moveCamera = checkPlayerPos();
+
         
     }
 
-    private IEnumerator followPlayer(Vector3 location)
+    void FixedUpdate()
     {
-        yield return new WaitForSeconds(delay); 
+        Vector3 desiredPosition;
+       
+        desiredPosition = player.transform.position + offset;
 
-        this.transform.position = new Vector3(location.x + offset.x, location.y + offset.y, zDist);
-
-    }
-
-    void LateUpdate()
-    {
-        /*
-         transform.position = camVec;
-         camVec.x = player.transform.position.x + offset.x;
-         camVec.y = player.transform.position.y + offset.y;
-         camVec.z = zDist;
-         */
         
-        if(player.GetComponent<PlayerController>().cameraTrigger)
-            StartCoroutine(followPlayer(player.transform.position));
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
 
-        //this.transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, zDist );
+        Vector3 speedPosition = Vector3.Lerp(transform.position, desiredPosition, speedSmoothSpeed * Time.deltaTime);
+
+        if(moveCamera)
+        {
+            transform.position = speedPosition;
+        }
+        else
+            transform.position = smoothPosition;
+        
     }
+
+    bool checkPlayerPos()
+    {
+
+        Vector3 playerPos = Camera.main.WorldToViewportPoint(player.transform.position);
+
+        if (playerPos.x > xMax || playerPos.x < xMin || playerPos.y > yMax || playerPos.y < yMin)
+            return true;
+        else
+            return false;
+        
+    }
+
 }
