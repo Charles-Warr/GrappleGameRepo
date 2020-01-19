86a5aa469d0329a4c0ba5b6872c7a2ba3cbe8327
Charles-Warr <cawarr@student.fullsail.edu>
2020-01-18 19:11:38 -0500

    Enemy Health Script, Updated Walker Prefab

---
+++ GrappleGame/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 1;
+
+    private GameObject self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = this.gameObject;
+    }
+
+    void checkHealth()
+    {
+        if(health <= 0)
+        {
+            self.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        checkHealth();
+    }
+}
