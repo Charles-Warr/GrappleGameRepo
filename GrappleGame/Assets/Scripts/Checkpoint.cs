c694b20a19e73de0732a75e330feeda1d605c5f5
Charles-Warr <cawarr@student.fullsail.edu>
2020-01-23 17:09:11 -0500

    checkpoint script, not finished

---
+++ GrappleGame/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public float checkPointNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
